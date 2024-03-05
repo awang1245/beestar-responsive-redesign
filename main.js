00074b6bb352228a9190553256d1bcfa419dc8b2
Anna Wang <anna_wang@brown.edu>
2024-03-05 01:13:53 -0500

    program section done

--- main.js
+++ main.js
@@ -1,24 +1,14 @@
 import "./style.css";
-import javascriptLogo from "./javascript.svg";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.js";
 
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
+function toggleExpand() {
+  const moreGrades = document.querySelector(".more-grades");
+  moreGrades.classList.toggle("active");
 
-// setupCounter(document.querySelector('#counter'))
+  const toggleButton = document.querySelector(".toggle");
+  toggleButton.classList.toggle(
+    "clicked",
+    moreGrades.classList.contains("active")
+  );
+}
+
+document.getElementById("toggle").addEventListener("click", toggleExpand);
